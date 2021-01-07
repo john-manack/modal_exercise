1f57a650b439c1819d1a3d9628f7d96477262622
John Manack <jmnck28@gmail.com>
2021-01-07 16:36:33 -0500

    full modal functionality using querySelector

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 const showModal = document.querySelector('#showModal');
+const closeModal = document.querySelector('#closeModal');
 
 showModal.addEventListener('click', function() {
+    const modalOverlay = document.querySelector("#overlay");
+    modalOverlay.classList.toggle("visible");
+});
+
+closeModal.addEventListener('click', function () {
     const modalOverlay = document.querySelector("#overlay");
     modalOverlay.classList.toggle("visible");
 });
